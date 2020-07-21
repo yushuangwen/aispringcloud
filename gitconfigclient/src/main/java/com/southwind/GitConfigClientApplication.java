c34677192e69cc39fd4dbfc7ae8b46b5834e1ce3
wen_global <2485537864@qq.com>
2020-07-21 08:32:03 +0800

    1

---
+++ gitconfigclient/src/main/java/com/southwind/GitConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.southwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GitConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GitConfigClientApplication.class,args);
+    }
+}
